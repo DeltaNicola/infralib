3515cee3f86bf5d3de02202b7796817d71dee4f9
DeltaNicola <delta.meloni@gmail.com>
2024-11-25 14:34:24 +0100

    LOG_FILE_PATH

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | ------- | ----- | ------- |
 | LOG_ON_CONSOLE | Enable logging on console | true |
 | LOG_ON_FILE | Enable logging on file | false | 
+| LOG_FILE_PATH | File path used for logging | ./app.log | 
 | LOG_ON_OPEN_SEARCH | Enable logging on OpenSearch | false |
 | OPEN_SEARCH_ENDPOINT | Endpoint to OpenSearch | / |
 | INDEX_NAME | Name of OpenSearch Index | / |
--- logger/file.go
+++ logger/file.go
@@ -1,14 +1,21 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func fileLoggerCore() zapcore.Core {
+	logFilePath, exists := os.LookupEnv("LOG_FILE_PATH")
+	if !exists {
+		logFilePath = "./app.log"
+	}
+
 	lumberjack := &lumberjack.Logger{
-		Filename:   "./logs/app.log",
+		Filename:   logFilePath,
 		MaxSize:    10, // In MB
 		MaxBackups: 3,
 		MaxAge:     28, // In days
